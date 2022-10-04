aca4ea08c6d20fa3b51dc00a4f59911bb08efdf7
Stephen Martinez <104386666+SdMartinez13@users.noreply.github.com>
2022-10-03 22:36:29 -0700

    lab 11

---
+++ chance.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const Chance = require('chance');
+
+const chance = new Chance();
+
+let person = {
+  store: chance.company(),
+  orderID: chance.guid(),
+  customer: chance.last(),
+  address: chance.address(),
+}
+
+console.log(person);
